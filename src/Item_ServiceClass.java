f8435c76cee339bfa764bb4aff5ca00f7fb057b8
AhmedElsisy <ahmedelsisy44@gmail.com>
2018-12-21 21:15:55 +0200

    Added Item classes

---
+++ src/Item_ServiceClass.java
@@ -0,0 +1,19 @@
+public class Item_ServiceClass {
+    private static Item item;
+
+    public static void Upload(){
+
+    }
+
+    public static void SelectingItem(){
+
+    }
+
+    public static void Search(){
+
+    }
+
+    public static boolean MarkingItem(){
+
+    }
+}
