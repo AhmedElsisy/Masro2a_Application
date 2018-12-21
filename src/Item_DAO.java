6ca9db352c4bbdd6be5f853b0fea2e4e3a6b1f43
AhmedElsisy <ahmedelsisy44@gmail.com>
2018-12-21 21:12:47 +0200

    Added Item classes

---
+++ src/Item_DAO.java
@@ -0,0 +1,40 @@
+import org.omg.CORBA.ARG_IN;
+
+import java.util.ArrayList;
+
+public class Item_DAO {
+    private static String serial;
+    private static boolean HasSerial;
+
+    public static void insertItem(Item item){
+
+    }
+
+    public static void updateItem(Item item){
+
+    }
+
+    public static void deleteItem(Item item){
+
+    }
+
+    public static ArrayList<Item> FindALL(){
+
+    }
+
+    public static ArrayList<Item> FindAllByCat(String x){
+
+    }
+
+    public static ArrayList<Item> FindAllByInfo(String x){
+
+    }
+
+    public static ArrayList<Item> FindAllByName(String x){
+
+    }
+
+    public static String FindSerial(String x){
+        return serial; // to be changed
+    }
+}
