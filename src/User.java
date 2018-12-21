aa977ad994e11e8c12fbaf4941a61a7bb0718ab0
AhmedElsisy <ahmedelsisy44@gmail.com>
2018-12-21 21:01:48 +0200

    Added User classes

--- src/User.java
+++ src/User.java
@@ -1,2 +1,51 @@
 public class User {
+    private static String FirstName , SecondName , Email , password;
+    private static int Age , PhoneNumber;
+
+    public static void setFirstName(String x){
+        FirstName = x;
+    }
+
+    public static void setSecondName(String x){
+        SecondName = x;
+    }
+
+    public static void setEmail(String email) {
+        Email = email;
+    }
+
+    public static void setAge(int age) {
+        Age = age;
+    }
+
+    public static void setPassword(String password) {
+        User.password = password;
+    }
+
+    public static void setPhoneNumber(int phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public static String getFirstName(){
+        return FirstName;
+    }
+
+    public static String getSecondName() {
+        return SecondName;
+    }
+
+    public static String getEmail() {
+        return Email;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+    public static int getAge() {
+        return Age;
+    }
+
+    public static int getPhoneNumber() {
+        return PhoneNumber;
+    }
 }
